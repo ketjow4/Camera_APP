5345fca83181fdd02e5335dba6a56bbe173abd96
Wojtek Dudzik <wojtek.adam.dudzik@gmail.com>
2015-10-07 17:17:45 +0200

    Added first version

---
+++ app/src/main/java/com/example/t430/camera_app/CameraActivity.java
@@ -0,0 +1,316 @@
+package com.example.t430.camera_app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by T430 on 2015-10-06.
+ */
+public class CameraActivity extends Activity {
+
+    private static Camera mCamera;
+    private static CameraPreview mPreview;
+
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
+    private boolean isRecording = false;
+
+    static Context context;
+
+    /** setting last image taken **/
+    static ImageView myImage;
+    static File imgFile = null;
+    int duration = Toast.LENGTH_LONG;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCamera = getCameraInstance();
+
+        context = getApplicationContext();
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+
+        myImage = (ImageView) findViewById(R.id.lastPic);
+        /** if last image is clicked it will open in an image viewer app **/
+        myImage.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                if (imgFile == null) {
+                    CharSequence text = "This is only an icon, take a photo";
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                } else {
+                    intent.setDataAndType(Uri.parse("file://" + imgFile.getAbsolutePath()), "image/*");
+                    startActivity(intent);
+                }
+                return false;
+            }
+        });
+
+
+
+        // Add a listener to the Capture button
+        Button captureButton = (Button) findViewById(R.id.button_capture2);
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (isRecording) {
+                            // stop recording and release camera
+                            mMediaRecorder.stop();  // stop the recording
+                            releaseMediaRecorder(); // release the MediaRecorder object
+                            mCamera.lock();         // take camera access back from MediaRecorder
+
+                            // inform the user that recording has stopped
+                            //setCaptureButtonText("Capture");
+                            isRecording = false;
+                        } else {
+                            // initialize video camera
+                            if (prepareVideoRecorder()) {
+                                // Camera is available and unlocked, MediaRecorder is prepared,
+                                // now you can start recording
+                                mMediaRecorder.start();
+
+                                // inform the user that recording has started
+                                //setCaptureButtonText("Stop");
+                                isRecording = true;
+                            } else {
+                                // prepare didn't work, release the camera
+                                releaseMediaRecorder();
+                                // inform user
+                            }
+                        }
+                    }
+                }
+        );
+
+    }
+
+
+    private void releaseMediaRecorder(){
+        if (mMediaRecorder != null) {
+            mMediaRecorder.reset();   // clear recorder configuration
+            mMediaRecorder.release(); // release the recorder object
+            mMediaRecorder = null;
+            mCamera.lock();           // lock camera for later use
+        }
+    }
+
+    /** Create a file Uri for saving an image or video */
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+        /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(0); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    public void takePhoto(View v){
+        mCamera.takePicture(null, null, mPicture);
+    }
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+
+            if (pictureFile == null){
+                Log.d("ERROR", "Error creating media file, check storage permissions:");
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+
+            } catch (FileNotFoundException e) {
+                Log.d("ERROR", "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d("ERROR", "Error accessing file: " + e.getMessage());
+            }
+            setImage();
+
+            mCamera.startPreview();
+        }
+    };
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    public static void setImage(){
+        if(imgFile !=null){
+            if(imgFile.exists()){
+                Bitmap myBitmap = decodeSampleImage(imgFile, 100, 100); // prevents memory out of memory exception
+                myImage.setImageBitmap(myBitmap);
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //releaseMediaRecorder();
+        releaseCamera();              // release the camera immediately on pause event
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mCamera == null){
+            mCamera = getCameraInstance();
+
+            context = getApplicationContext();
+            mPreview = new CameraPreview(this, mCamera);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
+        }
+    }
+
+    private void releaseCamera(){
+        if (mCamera != null){
+            mPreview.getHolder().removeCallback(mPreview);
+            mCamera.release();
+            mCamera = null;
+
+        }
+    }
+
+    public static Bitmap decodeSampleImage(File f, int width, int height) {
+        try {
+            System.gc(); // First of all free some memory 	        // Decode image size
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(new FileInputStream(f), null, o); 	        // The new size we want to scale to 	                            final int requiredWidth = width;
+            final int requiredHeight = height; 	        // Find the scale value (as a power of 2)
+            int sampleScaleSize = 1;
+
+            //while (o.outWidth / sampleScaleSize / 2 >= requiredWidth && o.outHeight / sampleScaleSize / 2 >= requiredHeight)
+              //  sampleScaleSize *= 2;
+
+            // Decode with inSampleSize
+
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize = sampleScaleSize;
+
+            return BitmapFactory.decodeStream(new FileInputStream(f), null, o2);
+        } catch (Exception e) {
+            //  Log.d(TAG, e.getMessage()); // We don't want the application to just throw an exception
+        }
+
+        return null;
+    }
+
+    private MediaRecorder mMediaRecorder = null;
+    private boolean prepareVideoRecorder(){
+
+        //mCamera = getCameraInstance();
+        mMediaRecorder = new MediaRecorder();
+
+        // Step 1: Unlock and set camera to MediaRecorder
+        mCamera.unlock();
+        mMediaRecorder.setCamera(mCamera);
+
+        // Step 2: Set sources
+        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+
+        // Step 3: Set a CamcorderProfile (requires API Level 8 or higher)
+        mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+
+        // Step 4: Set output file
+        mMediaRecorder.setOutputFile(getOutputMediaFile(MEDIA_TYPE_VIDEO).toString());
+
+        // Step 5: Set the preview output
+        mMediaRecorder.setPreviewDisplay(mPreview.getHolder().getSurface());
+
+        // Step 6: Prepare configured MediaRecorder
+        try {
+            mMediaRecorder.prepare();
+        } catch (IllegalStateException e) {
+            Log.d("dsadas", "IllegalStateException preparing MediaRecorder: " + e.getMessage());
+            releaseMediaRecorder();
+            return false;
+        } catch (IOException e) {
+            Log.d("dsadsa", "IOException preparing MediaRecorder: " + e.getMessage());
+            releaseMediaRecorder();
+            return false;
+        }
+        return true;
+    }
+}
